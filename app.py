878755aae91c5585eeed4e094d91bec3a760a425
Dafrawys <ahmeddafrawy123@gmail.com>
2024-01-30 00:29:49 +0200

    Last

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ model_path = Path(settings.DETECTION_MODEL)
 
 # Load Pre-trained ML Model
 try:
-    model = helper.load_model('Streamlit_Facial_Recognition/weights/best.pt')
+    model = helper.load_model(model_path)
 except Exception as ex:
     st.error(f"Unable to load model. Check the specified path: {model_path}")
     st.error(ex)
