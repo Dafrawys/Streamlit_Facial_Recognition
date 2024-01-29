0e96d709a5a65062f11a768cc85b964752219993
Dafrawys <ahmeddafrawy123@gmail.com>
2024-01-29 22:06:50 +0200

    last commit

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ model_path = Path(settings.DETECTION_MODEL)
 
 # Load Pre-trained ML Model
 try:
-    model = helper.load_model('C:\\Users\\DELL\\Desktop\\grad_project\\test\\weights\\best.pt')
+    model = helper.load_model('weights/best.pt')
 except Exception as ex:
     st.error(f"Unable to load model. Check the specified path: {model_path}")
     st.error(ex)
